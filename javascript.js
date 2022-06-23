39b5c1beb3d469cbe4b5e76caaabf6e36c2a4af9
Backley93 <branden.ackley@gmail.com>
2022-06-22 18:48:27 -0700

    Edited random() function to return the random number, added computerPlay() function to generate a rock paper or scissors string

--- javascript.js
+++ javascript.js
@@ -6,4 +6,28 @@ function random() {
     } else if (num === 10) {
         num = num - 1;
     }
+
+    return num;
+}
+
+function computerPlay() {
+    let num = random();
+    console.log(num);
+
+    let result = null;
+    console.log(result);
+
+    if (num >= 1 && num <= 3) {
+        result = 'rock';
+        console.log(result);
+        return result;
+    } else if (num >= 4 && num <= 6) {
+        result = 'paper';
+        console.log(result);
+        return result;
+    } else if (num >= 7 && num <= 9) {
+        result = 'scissors';
+        console.log(result);
+        return result;
+    }
 }
