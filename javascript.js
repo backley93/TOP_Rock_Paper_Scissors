462bdad935bfc5a3b761edd9bb9ad9133be196fa
Backley93 <branden.ackley@gmail.com>
2022-07-11 23:20:08 -0700

    Made changest to javascript and html
    In my javascript I removed the game() function as specified in the
    project requirements. I also added a function that uses an event
    listener to get a string that represents the player selection. The
    string is taken from the class of each button on the page.
    
    In my html I changed the classes of the buttons in order to represent
    the strings I need for the event listener function in my JS.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-/* This function generates a random number between 1 and 9 inclusive. */
+// This function generates a random number between 1 and 9 inclusive.
 
 function random() {
     let num = Math.round(Math.random()*10);
@@ -12,8 +12,8 @@ function random() {
     return num;
 }
 
-/* This function uses the helper function random() to determine if the computer
-     will select rock paper or scissors using conditional statements. */
+/*Function that returns the computer selection using the helper function
+random()*/
 
 function computerPlay() {
     let num = random();
@@ -21,16 +21,29 @@ function computerPlay() {
     let result = null;
 
     if (num >= 1 && num <= 3) {
-        result = 'rock';
+        result = 'ROCK';
         return result;
     } else if (num >= 4 && num <= 6) {
-        result = 'paper';
+        result = 'PAPER';
         return result;
     } else if (num >= 7 && num <= 9) {
-        result = 'scissors';
+        result = 'SCISSORS';
         return result;
     }
 }
+
+//Function to manipulate the dom and take player input from button elements
+
+const inputs = document.querySelectorAll('button');
+
+let playerInput = null;
+
+inputs.forEach(input => {
+    input.addEventListener('click', (e) => {
+        playerInput = input.className;
+        return playerInput;
+    })
+});
 
 /* This function takes two agruments, player selection which is driven by a 
     prompt in the game() function and computer selection which is returned in the 
@@ -39,82 +52,25 @@ function computerPlay() {
 
 
 function playRound(playerSelection, computerSelection) {
-    let plyr = playerSelection;
-
-    let comp = computerSelection;
-
+    playerSelection = playerInput.toUpperCase();
+    console.log(playerSelection);
+    computerSelection = computerPlay();
+    console.log(computerSelection);
     let result = null;
 
-    if ((plyr === 'ROCK' && comp === 'SCISSORS') || 
-        (plyr === 'PAPER' && comp === 'ROCK') || 
-        (plyr === 'SCISSORS' && comp === 'PAPER')) {
+    if ((playerSelection === 'ROCK' && computerSelection === 'SCISSORS') || 
+        (playerSelection === 'PAPER' && computerSelection === 'ROCK') || 
+        (playerSelection === 'SCISSORS' && computerSelection === 'PAPER')) {
         result = 'W';
         return result;
-    } else if ((plyr === 'ROCK' && comp === 'PAPER') || 
-        (plyr === 'PAPER' && comp === 'SCISSORS') || 
-        (plyr === 'SCISSORS' && comp === 'ROCK')) {
+    } else if ((playerSelection === 'ROCK' && computerSelection === 'PAPER') || 
+        (playerSelection === 'PAPER' && computerSelection === 'SCISSORS') || 
+        (playerSelection === 'SCISSORS' && computerSelection === 'ROCK')) {
         result = 'L';
         return result;
-    } else if (plyr === comp) {
+    } else if (playerSelection === computerSelection) {
         result = 'T';
         return result;
-    } else {
-        alert('Please enter rock, paper or scissors *case insensitive*');
-    }
-} 
-
-/* This function plays a 5 round game of rock paper scissors. Using the helper
-    function playRound() in combination with a for loop the function gets the result 
-    of each playRound() and publishes them as alerts. After 5 rounds the function 
-    determines if the player won, the computer won or if there was a tie. */
-
-function game() {
-    let pScore = 0;
-
-    let plyrPick = null;
-
-    let cScore = 0;
-
-    let compPick = null;
-
-    for (let i = 0; i < 5; i++) {
-        plyrPick = prompt().toUpperCase();
-        console.log(plyrPick);
-
-        if (plyrPick !== 'ROCK' && plyrPick !== 'PAPER' && plyrPick != 'SCISSORS') {
-            alert('You must enter rock, paper or scissors.');
-            break;
-        }
-
-        compPick = computerPlay().toUpperCase();
-        console.log(compPick);
-
-        if (compPick !== 'ROCK' && compPick !== 'PAPER' && compPick !== 'SCISSORS') {
-            alert('We\'re experiencing technical difficulties please try the game again.');
-            break;
-        }
-
-        if (playRound(plyrPick, compPick) === 'W') {
-            pScore = pScore + 1;
-        } else if (playRound(plyrPick, compPick) === 'L') {
-            cScore = cScore + 1;
-        }
-
-        alert(`Player picked\: ${plyrPick} and the computer picked\: ${compPick}.`);
-
-        alert(`The score is, Player\: ${pScore} to Computer\: ${cScore}.`);
-    }
-
-    if (pScore > cScore) {
-        return alert(`Congratulations! You win by a score of ${pScore} to ${cScore}.`);
-    }
-
-    if (cScore > pScore) {
-        return alert(`Sorry, you lost to the computer by a score of ${cScore} to ${pScore}.`);
-    }
-
-    if (pScore == cScore) {
-        return alert(`Gasp! It\'s a tie by a score of ${pScore} to ${cScore}.`);
     }
 }
 
